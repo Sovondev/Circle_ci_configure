1e03f8540e83a93fed07e4b8e1971593a137e3a1
Sovondev <sovondevmondal@gmail.com>
2020-08-22 19:19:36 +0100

    circleci

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-main(){
+int main(){
     int a,b,c = 0;
     a = 24;
     b = 24;
     c = a+b;
     printf("Hello world");
-    print("%d",c);
+    printf("%d",c);
     return 0;
 }
